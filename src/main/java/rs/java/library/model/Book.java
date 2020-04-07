ae30b3ec93d4ad9c339a8a11257ee96860860911
rsjavasolutions <stan.robert.1986@gmail.com>
2020-04-07 10:04:20 +0200

    New class Book

---
+++ src/main/java/rs/java/library/model/Book.java
@@ -0,0 +1,58 @@
+package rs.java.library.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "books")
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name ="book_id")
+    private Integer id;
+    private String isbn;
+    private String author;
+    private String title;
+
+    public Book() {
+    }
+
+    public Book(Integer id, String isbn, String author, String title) {
+        this.id = id;
+        this.isbn = isbn;
+        this.author = author;
+        this.title = title;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}

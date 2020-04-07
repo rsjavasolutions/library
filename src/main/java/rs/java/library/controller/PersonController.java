f85e8bb9536b850f88f4d791d82768eecbe03819
rsjavasolutions <stan.robert.1986@gmail.com>
2020-04-07 12:27:35 +0200

    New class PersonController

---
+++ src/main/java/rs/java/library/controller/PersonController.java
@@ -0,0 +1,27 @@
+package rs.java.library.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import rs.java.library.model.Person;
+import rs.java.library.service.PersonService;
+
+@RestController
+@RequestMapping("/api/")
+public class PersonController {
+
+    PersonService personService;
+
+    @Autowired
+    public PersonController(PersonService personService) {
+        this.personService = personService;
+    }
+
+    @PostMapping("/people")
+    public Person addPerson(@RequestBody Person person){
+        return personService.addPerson(person);
+    }
+
+}

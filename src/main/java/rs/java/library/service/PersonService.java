5199db25d0cd7c4a17fc20dcb7d327f681fb0e2c
rsjavasolutions <stan.robert.1986@gmail.com>
2020-04-07 12:24:42 +0200

    New class PersonService

--- src/main/java/rs/java/library/service/PersonService.java
+++ src/main/java/rs/java/library/service/PersonService.java
@@ -1,4 +1,21 @@
 package rs.java.library.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import rs.java.library.model.Person;
+import rs.java.library.repository.PersonRepository;
+
+@Service
 public class PersonService {
+
+    PersonRepository personRepository;
+
+    @Autowired
+    public PersonService(PersonRepository personRepository) {
+        this.personRepository = personRepository;
+    }
+
+    public Person addPerson(Person person) {
+        return personRepository.save(person);
+    }
 }

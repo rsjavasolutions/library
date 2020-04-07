b218263a8b7f2a74fbdd11bf96a76f1e5008af26
rsjavasolutions <stan.robert.1986@gmail.com>
2020-04-07 13:09:12 +0200

    New class LoanService

--- src/main/java/rs/java/library/service/LoanService.java
+++ src/main/java/rs/java/library/service/LoanService.java
@@ -1,5 +1,21 @@
 package rs.java.library.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import rs.java.library.model.Loan;
+import rs.java.library.repository.LoanRepository;
 
+@Service
 public class LoanService {
+
+    LoanRepository loanRepository;
+
+    @Autowired
+    public LoanService(LoanRepository loanRepository) {
+        this.loanRepository = loanRepository;
+    }
+
+    public Loan addLoan(Loan loan){
+        return loanRepository.save(loan);
+    }
 }

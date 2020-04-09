abd14b8ca244bf4bf47941db121343617bcf5dfa
rsjavasolutions <stan.robert.1986@gmail.com>
2020-04-09 20:02:15 +0200

    New method in PersonService class - updatePerson

--- src/main/java/rs/java/library/service/PersonService.java
+++ src/main/java/rs/java/library/service/PersonService.java
@@ -1,6 +1,8 @@
 package rs.java.library.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import rs.java.library.exception.InvalidIdException;
 import rs.java.library.model.Person;
@@ -29,4 +31,14 @@ public class PersonService {
         }
         throw new InvalidIdException("INVALID PERSON ID NUMBER");
     }
+
+    public ResponseEntity<Person> updatePerson(Integer id, Person person){
+        Optional<Person> optionalPerson = personRepository.findById(id);
+        if (optionalPerson.isPresent()){
+            optionalPerson = Optional.of(person);
+            return new ResponseEntity<>(HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+    }
+
 }
